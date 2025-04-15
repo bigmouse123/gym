f742a1459306ee2d5f16578995d50ad0ad844bf5
lijiankun <1732931774@qq.com>
2025-04-15 12:34:16 +0800

    admin删除、添加、更新、图片上传

---
+++ gym-admin/src/main/java/com/jiankun/gym/controller/UploadController.java
@@ -0,0 +1,49 @@
+package com.jiankun.gym.controller;
+
+import com.jiankun.gym.util.AliOSSUtil;
+import com.jiankun.gym.util.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+/**
+ * @author OfferKing
+ * @version 1.0
+ * @date 2025/3/8 23:12
+ */
+@Controller
+public class UploadController {
+    @Autowired
+    private AliOSSUtil aliOSSUtil;
+
+    @RequestMapping("/upload")
+    @ResponseBody
+    public Result upload(MultipartFile file) {
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        String fileName = file.getOriginalFilename();
+        String extension = fileName.substring(fileName.lastIndexOf("."));
+        String newFileName = uuid + extension;
+        //1.本地上传
+//        String filePath = "F:\\mypic\\" + newFileName;
+//        try {
+//            file.transferTo(new File(filePath));
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        return Result.ok("上传成功", newFileName);
+
+        //2.阿里云上传
+        String url = "";
+        try {
+            url = aliOSSUtil.uploadFile(newFileName, file.getInputStream());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("上传图片:" + url);
+        return Result.ok("上传成功", url);
+    }
+}
--- ui/admin-ui/src/views/Admin.vue
+++ ui/admin-ui/src/views/Admin.vue
@@ -2,6 +2,7 @@
     import adminApi from "@/api/admin.js";
     import {reactive, ref} from 'vue'
     import {ElMessage, ElMessageBox} from "element-plus";
+    import {Plus} from '@element-plus/icons-vue'
 
     const list = ref([0]);
     const total = ref(0);
@@ -21,6 +22,7 @@
     //     })
     // }
 
+    //查询
     const loadData = () => {
         adminApi.list(adminQuery).then(result => {
             if (result.code == 0) {
@@ -30,6 +32,7 @@
         })
     }
 
+    //单行删除
     const deleteById = (id) => {
         ElMessageBox.confirm(
             '你确定要删除吗?',
@@ -52,48 +55,206 @@
                 })
             })
     }
+
+
+    //批量删除
+    const ids = ref([]);
+    const handleSelectChange = (rows) => {
+        console.log("多选:" + rows)
+        ids.value = rows.map(row => row.id)
+        console.log("ids=" + ids);
+        console.log("ids.value=" + ids.value);
+    }
+    const deleteAll = () => {
+        ElMessageBox.confirm(
+            '你确定要删除吗?',
+            '警告',
+            {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning',
+                lockScroll: false
+            }
+        )
+            .then(() => {
+                adminApi.deleteAll(ids.value).then(result => {
+                    if (result.code == 0) {
+                        ElMessage.success(result.msg);
+                        loadData();
+                    } else {
+                        ElMessage.error(result.msg);
+                    }
+                })
+            })
+    }
+
+    //添加、编辑
+    const dialogFormVisible = ref(false);
+    const admin = ref({})
+    const title = ref();
+    const showAddDialog = () => {
+        dialogFormVisible.value = true;
+        admin.value = {};
+        title.value = "添加";
+    }
+
+    const showUpdateDialog = (id) => {
+        dialogFormVisible.value = true;
+        admin.value = {};
+        title.value = "编辑";
+        adminApi.selectById(id).then(result => {
+            if (result.code == 0) {
+                admin.value = result.data;
+            }
+        })
+    }
+    const addOrUpdate = () => {
+        if (admin.value.id) {
+            adminApi.update(admin.value).then(result => {
+                if (result.code == 0) {
+                    ElMessage.success(result.msg)
+                    dialogFormVisible.value = false
+                    loadData()
+                } else {
+                    ElMessage.error(result.msg)
+                }
+            })
+        } else {
+            adminApi.add(admin.value).then(result => {
+                if (result.code == 0) {
+                    ElMessage.success(result.msg)
+                    dialogFormVisible.value = false
+                    loadData()
+                } else {
+                    ElMessage.error(result.msg)
+                }
+            })
+        }
+    }
+
+    //图片上传
+    const handleAvatarSuccess = (result) => {
+        admin.value.avatar = result.data
+    }
+
     loadData();
 </script>
 
 <template>
-    <el-form :inline="true" :model="formInline" class="demo-form-inline">
-        <el-form-item label="姓名">
-            <el-input v-model="adminQuery.name" placeholder="请输入姓名"></el-input>
-        </el-form-item>
-        <el-form-item label="电话">
-            <el-input v-model="adminQuery.phone" placeholder="请输入电话"></el-input>
-        </el-form-item>
-        <el-form-item>
-            <el-button type="primary" @click="loadData">查询</el-button>
-        </el-form-item>
-    </el-form>
-    <el-table :data="list" style="width: 100%">
-        <el-table-column type="selection" width="55"/>
-        <el-table-column fixed prop="id" label="ID"/>
-        <el-table-column prop="name" label="名字"/>
-        <el-table-column prop="password" label="密码"/>
-        <el-table-column prop="phone" label="电话"/>
-        <el-table-column prop="email" label="邮箱"/>
-        <el-table-column prop="image" label="头像"/>
-        <el-table-column prop="updateTime" label="更新时间"/>
-        <el-table-column fixed="right" label="Operations">
-            <template #default="{ row }">
-                <el-button type="primary" @click="showUpdateDialog(row.id)">编辑</el-button>
-                <el-button type="danger" @click="deleteById(row.id)">删除</el-button>
-            </template>
-        </el-table-column>
-    </el-table>
-    <el-pagination
-        v-model:current-page="adminQuery.page"
-        v-model:page-size="adminQuery.limit"
-        :page-sizes="[10, 20, 30, 40]"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="total"
-        @change="loadData"
-        style="margin-top: 20px; justify-content: flex-end"
-    />
+    <el-card>
+        <template #header>
+            <div class="header">
+                <el-button type="primary" @click="showAddDialog">添加</el-button>
+                <el-button type="primary" @click="deleteAll">批量删除</el-button>
+            </div>
+        </template>
+        <el-form :inline="true" :model="formInline" class="demo-form-inline">
+            <el-form-item label="姓名">
+                <el-input v-model="adminQuery.name" placeholder="请输入姓名"></el-input>
+            </el-form-item>
+            <el-form-item label="电话">
+                <el-input v-model="adminQuery.phone" placeholder="请输入电话"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="loadData">查询</el-button>
+            </el-form-item>
+        </el-form>
+        <el-table :data="list" style="width: 100%" ref="multipleTableRef" @selection-change="handleSelectChange">
+            <el-table-column type="selection" width="55"/>
+            <el-table-column fixed prop="id" label="ID"/>
+            <el-table-column prop="name" label="名字"/>
+            <el-table-column prop="password" label="密码"/>
+            <el-table-column prop="phone" label="电话"/>
+            <el-table-column prop="email" label="邮箱"/>
+            <!--<el-table-column prop="image" label="头像"/>-->
+            <el-table-column prop="avatar" label="头像">
+                <template #default="scope">
+                    <img :src="scope.row.avatar" style="max-height: 40px; max-width: 120px;"/>
+                </template>
+            </el-table-column>
+            <el-table-column prop="updateTime" label="更新时间"/>
+            <el-table-column fixed="right" label="Operations">
+                <template #default="{ row }">
+                    <el-button type="primary" @click="showUpdateDialog(row.id)">编辑</el-button>
+                    <el-button type="danger" @click="deleteById(row.id)">删除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <el-pagination
+            v-model:current-page="adminQuery.page"
+            v-model:page-size="adminQuery.limit"
+            :page-sizes="[10, 20, 30, 40]"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="total"
+            @change="loadData"
+            style="margin-top: 20px; justify-content: flex-end"
+        />
+    </el-card>
+
+    <!--添加、编辑弹出框-->
+    <el-dialog v-model="dialogFormVisible" :title="title" width="500" :lock-scroll="false">
+        <el-form :model="admin">
+            <el-form-item label="姓名" :label-width="50">
+                <el-input v-model="admin.name" autocomplete="off"/>
+            </el-form-item>
+            <el-form-item label="密码" :label-width="50">
+                <el-input v-model="admin.password" autocomplete="off"/>
+            </el-form-item>
+            <el-form-item label="邮箱" :label-width="50">
+                <el-input v-model="admin.email" autocomplete="off"/>
+            </el-form-item>
+            <el-form-item label="手机" :label-width="50">
+                <el-input v-model="admin.phone" autocomplete="off"/>
+            </el-form-item>
+            <el-form-item label="头像" :label-width="50">
+                <el-upload
+                    class="avatar-uploader"
+                    action="/api/upload"
+                    :show-file-list="false"
+                    :on-success="handleAvatarSuccess">
+                    <img v-if="admin.avatar" :src="admin.avatar" class="avatar" />
+                    <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+                </el-upload>
+            </el-form-item>
+        </el-form>
+        <template #footer>
+            <div class="dialog-footer">
+                <el-button @click="dialogFormVisible = false">取消</el-button>
+                <el-button type="primary" @click="addOrUpdate">
+                    确定
+                </el-button>
+            </div>
+        </template>
+    </el-dialog>
 </template>
 
 <style scoped>
+    .avatar-uploader .avatar {
+        width: 178px;
+        height: 178px;
+        display: block;
+    }
+</style>
 
+<style>
+    .avatar-uploader .el-upload {
+        border: 1px dashed var(--el-border-color);
+        border-radius: 6px;
+        cursor: pointer;
+        position: relative;
+        overflow: hidden;
+        transition: var(--el-transition-duration-fast);
+    }
+
+    .avatar-uploader .el-upload:hover {
+        border-color: var(--el-color-primary);
+    }
+
+    .el-icon.avatar-uploader-icon {
+        font-size: 28px;
+        color: #8c939d;
+        width: 178px;
+        height: 178px;
+        text-align: center;
+    }
 </style>
